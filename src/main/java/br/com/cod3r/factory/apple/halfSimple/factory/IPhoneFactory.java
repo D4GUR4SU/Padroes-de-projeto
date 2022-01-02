8154eac1492bf7be769e234379d7c3af98ca6fa9
Dagurasu56 <douglas_search@hotmail.com>
2022-01-02 12:23:23 -0300

    projeto base sem os padroes implementados

---
+++ src/main/java/br/com/cod3r/factory/apple/halfSimple/factory/IPhoneFactory.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.factory.apple.halfSimple.factory;
+
+public abstract class IPhoneFactory {
+
+	
+}
---
+++ src/main/java/br/com/cod3r/factory/apple/simple/factory/IPhoneFactory.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.factory.apple.simple.factory;
+
+public abstract class IPhoneFactory {
+
+	
+}
