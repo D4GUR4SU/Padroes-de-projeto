be05d4df3c64ec005474584ba3032a7c7eb5762b
Dagurasu56 <douglas_search@hotmail.com>
2022-01-06 17:31:45 -0300

    Factory method utilizando metodos de consulta e update dentro dos bancos de dados Oracle e PostgreSQL

--- src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,20 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.OracleFactory;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresFactory;
+
 public class Client {
 
 	public static void main(String[] args) {
-
+		
+		DB oracle = new OracleFactory().getDatabase();
+		DB postgres = new PostgresFactory().getDatabase();
+		
+		oracle.query("SELECT * FROM users");
+		oracle.update("INSERT INTO users VALUES ('User', 25)");
+		
+		postgres.query("SELECT * FROM users");
+		postgres.update("INSERT INTO users VALUES ('User', 25)");
 	}
 }
