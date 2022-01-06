251ee7c447daf61bcee1600fcefc8910f873b2b9
Dagurasu56 <douglas_search@hotmail.com>
2022-01-06 19:35:43 -0300

    implementando segundo exemplo do padrao abstract factory utilizando um serviço REST e um EJB para criar, atualizar e deletar um usuário ou um carro

--- src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
+++ src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
@@ -1,8 +1,22 @@
 package br.com.cod3r.abstractfactory.app.service;
 
+import br.com.cod3r.abstractfactory.app.service.factory.EJBAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.ServicesAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public class Client {
 
 	public static void main(String[] args) {
 
+		ServicesAbstractFactory factory = new EJBAbstractFactory();
+		
+		UserService userService = factory.getUserService();
+		userService.save("Douglas");
+		userService.delete(5);
+		
+		CarService carService = factory.getCarService();
+		carService.save("Sorento");
+		carService.update("BMW X5");
 	}
 }
