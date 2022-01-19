947a20a6ca3b9bde4bf7615e9bd36db7c88a33c2
Dagurasu56 <douglas_search@hotmail.com>
2022-01-18 21:09:13 -0300

    Implementando o padrao criacional builder para construir objetos complexos

--- src/main/java/br/com/cod3r/builder/meal/Client.java
+++ src/main/java/br/com/cod3r/builder/meal/Client.java
@@ -1,8 +1,27 @@
 package br.com.cod3r.builder.meal;
 
+import br.com.cod3r.builder.meal.builder.FastFoodMealBuilder;
+import br.com.cod3r.builder.meal.builder.FatMealBuilder;
+import br.com.cod3r.builder.meal.builder.JustFriesBuilder;
+import br.com.cod3r.builder.meal.builder.Menu1Builder;
+import br.com.cod3r.builder.meal.builder.Menu2Builder;
+import br.com.cod3r.builder.meal.director.MealDirector;
+
 public class Client {
 	
+	public static void order(String name, FastFoodMealBuilder builder) {
+		System.out.println("Ordering a " + name);
+		MealDirector director = new MealDirector(builder);
+		director.constructCombo();
+		System.out.println(director.getCombo());
+		
+		System.out.println("-----------------");
+	}
+	
 	public static void main(String[] args) {
-
+		order("Burger", new Menu1Builder());
+		order("Veggie Burger", new Menu2Builder());
+		order("Just Fries", new JustFriesBuilder());
+		order("Heart Attack", new FatMealBuilder());
 	}
 }
