57c0293229ffc7941788bf2c62e8c45f996620cc
Dagurasu56 <douglas_search@hotmail.com>
2022-01-19 23:43:42 -0300

    Implementando o padrao adapter, fazendo com que classes incompativeis trabalhe juntas

--- estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
@@ -1,6 +1,9 @@
 package br.com.cod3r.adapter.tvPort;
 
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToOldMonitorAdapter;
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToVGAAdapter;
 import br.com.cod3r.adapter.tvPort.devices.Computer;
+import br.com.cod3r.adapter.tvPort.devices.OldMonitor;
 import br.com.cod3r.adapter.tvPort.devices.TV;
 
 public class Client {
@@ -10,13 +13,22 @@ public class Client {
 		TV tv = new TV();
 		pc.connectPort(tv);
 		pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
-		
-		System.out.println("------ Monitor ----------");
-		
 
+		System.out.println("------ Monitor ----------");
+
+		Computer pc2 = new Computer();
+		OldMonitor monitor = new OldMonitor();
+
+		// pc2.connectPort(monitor); // nao funciona, pois o monitor nao implementa a interface HDMI
+		pc2.connectPort(new HDMIToVGAAdapter(monitor));
+		pc2.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 
 		System.out.println("------ Monitor Class Adapter ----------");
-		
+
+		Computer pc3 = new Computer();
+		HDMIToOldMonitorAdapter monitorAdapter = new HDMIToOldMonitorAdapter();
+		pc3.connectPort(monitorAdapter);
+		pc3.sendImageAndSound("Cat an rainbow", "Nyan cat song");
 
 	}
 }
