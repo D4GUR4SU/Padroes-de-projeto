68b450f6f1b637613e3625edac245535300ca3bd
Dagurasu56 <douglas_search@hotmail.com>
2022-01-20 17:49:05 -0300

    Implementando o padrao bridge, para separar a abstração da implementação

--- estructural-patterns/src/main/java/br/com/cod3r/bridge/converter/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/bridge/converter/Client.java
@@ -1,8 +1,24 @@
 package br.com.cod3r.bridge.converter;
 
+import br.com.cod3r.bridge.converter.converters.CSVConverter;
+import br.com.cod3r.bridge.converter.converters.Converter;
+import br.com.cod3r.bridge.converter.converters.JsonConverter;
+import br.com.cod3r.bridge.converter.employees.ITGuy;
+import br.com.cod3r.bridge.converter.employees.ProjectManager;
+
 public class Client {
 
 	public static void main(String[] args) {
 		
+		Converter csvConverter = new CSVConverter();
+		Converter jsonConverter = new JsonConverter();
+		
+		ITGuy it = new ITGuy("Elliot Alderson", 32, 4000d);
+		ProjectManager pm = new ProjectManager("James Gosling", 40, 6000d);
+		
+		System.out.println(csvConverter.getEmployeeFormated(it));
+		System.out.println(jsonConverter.getEmployeeFormated(it));
+		System.err.println(csvConverter.getEmployeeFormated(pm));
+		System.out.println(jsonConverter.getEmployeeFormated(pm));
 	}
 }
