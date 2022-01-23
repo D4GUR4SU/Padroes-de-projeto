b2f966e79aaffd25a20830b2cd1f9963297f7784
Dagurasu56 <douglas_search@hotmail.com>
2022-01-23 20:50:43 -0300

    Implementando o padrão iterator, percorrendo elementos de uma coleção sem expor as representações dele

--- behavioral-patterns/src/main/java/br/com/cod3r/iterator/tvChannel/after/TV.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/iterator/tvChannel/after/TV.java
@@ -1,16 +1,17 @@
-package br.com.cod3r.iterator.tvChannel;
+package br.com.cod3r.iterator.tvChannel.after;
 
+import java.util.Iterator;
 import java.util.Random;
 
-public class TV {
+public class TV implements Iterable<Integer> {
 	private Integer[] channels;
-	
+
 	public void searchAvaiableChannels() {
 		this.channels = new Integer[30];
 		Random r = new Random();
 		int channelsCount = 0;
-		for(int i = 0; i < channels.length; i++) {
-			if(r.nextInt(2) == 1) {
+		for (int i = 0; i < channels.length; i++) {
+			if (r.nextInt(2) == 1) {
 				channels[channelsCount++] = i;
 			}
 		}
@@ -19,12 +20,17 @@ public class TV {
 
 	public void accessChannel(Integer number) {
 		System.out.print(String.format("Channel %d: ", number));
-		for(int i = 0; i < channels.length; i++) {
-			if(channels[i] == number) {
+		for (int i = 0; i < channels.length; i++) {
+			if (channels[i] == number) {
 				System.out.println("OK!");
 				return;
 			}
 		}
 		System.out.println("No signal!");
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		return new ChannelIterator(channels);
 	}
 }
