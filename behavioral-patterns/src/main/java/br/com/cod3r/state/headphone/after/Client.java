0ecd6ff3ae23c382daee5e6455ab7c9b73e76404
Dagurasu56 <douglas_search@hotmail.com>
2022-01-23 23:39:25 -0300

    Implmentando o padrão state, permitindo que um objeto altere seu comportamento quando seu estado interno muda

--- behavioral-patterns/src/main/java/br/com/cod3r/state/headphone/after/Client.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/state/headphone/after/Client.java
@@ -1,4 +1,4 @@
-package br.com.cod3r.state.headphone;
+package br.com.cod3r.state.headphone.after;
 
 public class Client {
 
---
+++ behavioral-patterns/src/main/java/br/com/cod3r/state/headphone/after/states/HPState.java
@@ -0,0 +1,10 @@
+package br.com.cod3r.state.headphone.after.states;
+
+import br.com.cod3r.state.headphone.after.HeadPhone;
+
+public interface HPState {
+
+	void click(HeadPhone hp);
+
+	void longClick(HeadPhone hp);
+}
